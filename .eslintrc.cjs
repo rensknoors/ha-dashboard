04d0421ef864622f79066e0f372ca118b071c8ce
rensknoors <rensknoors@gmail.com>
2023-08-13 20:39:43 +0200

    chore: add more strict linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,21 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  include: ["./deploy.ts"],
   plugins: ['react-refresh', 'unused-imports'],
   rules: {
     'react-refresh/only-export-components': [
