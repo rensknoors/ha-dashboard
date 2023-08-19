054fddf8cdc576cbe2c5ec67695765468fe079a4
rensknoors <rensknoors@gmail.com>
2023-08-19 19:31:41 +0200

    feat: added default font Poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: [
+        'Poppins',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+      ],
+    },
     extend: {},
   },
   plugins: [],
