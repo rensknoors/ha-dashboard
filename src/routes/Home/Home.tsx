36c18a2703d481511261fded9e65bd4715e3a1cf
rensknoors <rensknoors@gmail.com>
2023-08-21 21:14:08 +0200

    style: added padding to time

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className="flex h-full w-full gap-6">
       <div className="basis-1/2">
-        <div className="centered-row">
+        <div className="centered-row py-8">
           <Time />
         </div>
         <div className="centered-row">
