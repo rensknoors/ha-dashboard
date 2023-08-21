e8eee4a21d6459c5d01f2b0202369dfcf043667f
rensknoors <rensknoors@gmail.com>
2023-08-21 21:12:57 +0200

    feat: replaced static icons with animated icons

--- src/components/atoms/Weather/Weather.tsx
+++ src/components/atoms/Weather/Weather.tsx
@@ -1,39 +1,41 @@
 import { useQuery } from '@tanstack/react-query';
+import Lottie from 'lottie-react';
 
-import { ReactComponent as CloudRainSingleIcon } from '@/assets/icons/cloud-rain-single.svg';
-import { ReactComponent as CloudRainIcon } from '@/assets/icons/cloud-rain.svg';
-import { ReactComponent as CloudIcon } from '@/assets/icons/cloud.svg';
-import { ReactComponent as CloudsIcon } from '@/assets/icons/clouds.svg';
-import { ReactComponent as FogIcon } from '@/assets/icons/fog.svg';
-import { ReactComponent as HailIcon } from '@/assets/icons/hail.svg';
-import { ReactComponent as LightningIcon } from '@/assets/icons/lightning.svg';
-import { ReactComponent as MoonCloudIcon } from '@/assets/icons/moon-cloud.svg';
-import { ReactComponent as MoonFogIcon } from '@/assets/icons/moon-fog.svg';
-import { ReactComponent as MoonWaxingCrescentIcon } from '@/assets/icons/moon-waxing-crescent.svg';
-import { ReactComponent as SnowflakeIcon } from '@/assets/icons/snowflake.svg';
-import { ReactComponent as SunCloudRainIcon } from '@/assets/icons/sun-cloud-rain.svg';
-import { ReactComponent as SunCloudIcon } from '@/assets/icons/sun-cloud.svg';
-import { ReactComponent as SunIcon } from '@/assets/icons/sun.svg';
+import clearDay from '@/assets/weather-lottie-animations/clear-day.json';
+import cloudy from '@/assets/weather-lottie-animations/cloudy.json';
+import drizzle from '@/assets/weather-lottie-animations/drizzle.json';
+import fogNight from '@/assets/weather-lottie-animations/fog-night.json';
+import hail from '@/assets/weather-lottie-animations/hail.json';
+import fog from '@/assets/weather-lottie-animations/mist.json';
+import moonWaxingCrescent from '@/assets/weather-lottie-animations/moon-waxing-crescent.json';
+import overcastNight from '@/assets/weather-lottie-animations/overcast-night.json';
+import overcast from '@/assets/weather-lottie-animations/overcast.json';
+import partlyCloudyDayRain from '@/assets/weather-lottie-animations/partly-cloudy-day-rain.json';
+import partlyCloudyDay from '@/assets/weather-lottie-animations/partly-cloudy-day.json';
+import partlyCloudyMoon from '@/assets/weather-lottie-animations/partly-cloudy-night.json';
+import rain from '@/assets/weather-lottie-animations/rain.json';
+import snow from '@/assets/weather-lottie-animations/snow.json';
+import thunderStormsRain from '@/assets/weather-lottie-animations/thunderstorms-rain.json';
 
 const LATLONG = '50.9953,5.9016';
 
 export const weatherStatesMap = {
-  zonnig: SunIcon,
-  bliksem: LightningIcon,
-  regen: CloudRainIcon,
-  buien: CloudRainSingleIcon,
-  hagel: HailIcon,
-  mist: FogIcon,
-  sneeuw: SnowflakeIcon,
-  bewolkt: CloudIcon,
-  lichtbewolkt: SunCloudIcon,
-  halfbewolkt: SunCloudIcon,
-  halfbewolkt_regen: SunCloudRainIcon,
-  zwaarbewolkt: CloudsIcon,
-  nachtmist: MoonFogIcon,
-  helderenacht: MoonWaxingCrescentIcon,
-  nachtbewolkt: MoonCloudIcon,
-  wolkennacht: MoonCloudIcon,
+  zonnig: clearDay,
+  bliksem: thunderStormsRain,
+  regen: drizzle,
+  buien: rain,
+  hagel: hail,
+  mist: fog,
+  sneeuw: snow,
+  bewolkt: cloudy,
+  lichtbewolkt: partlyCloudyDay,
+  halfbewolkt: partlyCloudyDay,
+  halfbewolkt_regen: partlyCloudyDayRain,
+  zwaarbewolkt: overcast,
+  nachtmist: fogNight,
+  helderenacht: moonWaxingCrescent,
+  nachtbewolkt: partlyCloudyMoon,
+  wolkennacht: overcastNight,
 } as const;
 
 type WeatherImage = keyof typeof weatherStatesMap;
@@ -56,9 +58,8 @@ const Weather = () => {
     refetchInterval: 1000 * 60 * 10, // 10 minutes
   });
   const image: WeatherImage = data?.liveweer[0]?.image;
-  console.log(image);
 
-  const WeatherIcon = weatherStatesMap[image] ?? null;
+  const icon = weatherStatesMap[image] ?? null;
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -74,8 +75,10 @@ const Weather = () => {
           <div>{data.liveweer[0]?.alarmtekst}</div>
         </div>
       )}
-      <div className="flex items-center text-3xl">
-        {WeatherIcon && <WeatherIcon fill="white" width={64} height={64} />}
+      <div className="flex items-center text-3xl gap-4">
+        {icon && (
+          <Lottie className="w-24 h-w-24" animationData={icon} loop={true} />
+        )}
         <div>{`${data.liveweer[0]?.temp} °C`}</div>
       </div>
     </div>
