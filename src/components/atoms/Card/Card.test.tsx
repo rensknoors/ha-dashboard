ffe24e4103e97a35c9e00ec4daae0596a4464753
rensknoors <rensknoors@gmail.com>
2023-08-28 23:50:12 +0200

    feat: added onLongPress to Card

---
+++ src/components/atoms/Card/Card.test.tsx
@@ -0,0 +1,44 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import { Card } from './Card';
+
+vi.useFakeTimers();
+
+const mockOnClick = vi.fn();
+const mockOnLongPress = vi.fn();
+
+const defaultProps = {
+  onClick: mockOnClick,
+  onLongPress: mockOnLongPress,
+};
+
+describe('Card', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('calls onClick when short pressed', () => {
+    render(<Card {...defaultProps}>Hello world</Card>);
+
+    const card = screen.getByText('Hello world');
+    fireEvent.click(card);
+
+    expect(mockOnClick).toHaveBeenCalledTimes(1);
+    expect(mockOnLongPress).not.toHaveBeenCalled();
+  });
+
+  it('calls onLongPress when long pressed, not onClick', () => {
+    render(<Card {...defaultProps}>Hello world</Card>);
+
+    const card = screen.getByText('Hello world');
+
+    fireEvent.mouseDown(card);
+
+    vi.advanceTimersByTime(1000);
+
+    fireEvent.mouseUp(card);
+
+    expect(mockOnLongPress).toHaveBeenCalledTimes(1);
+    expect(mockOnClick).not.toHaveBeenCalled();
+  });
+});
--- src/components/atoms/Card/Card.tsx
+++ src/components/atoms/Card/Card.tsx
@@ -1,15 +1,58 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useRef, useState } from 'react';
 
 export type CardProps = {
   children?: ReactNode;
+  onClick?: () => void;
+  onLongPress?: () => void;
 } & React.HTMLAttributes<HTMLDivElement>;
 
-const Card = ({ children, className, style, onClick }: CardProps) => {
+const Card = ({
+  children,
+  className,
+  style,
+  onClick,
+  onLongPress,
+}: CardProps) => {
+  const pressTimer = useRef<NodeJS.Timeout | null>(null);
+  const [longPressTriggered, setLongPressTriggered] = useState(false);
+
+  const startPressTimer = () => {
+    setLongPressTriggered(false);
+    if (onLongPress) {
+      pressTimer.current = setTimeout(() => {
+        onLongPress();
+        setLongPressTriggered(true);
+      }, 1000);
+    }
+  };
+
+  const stopPressTimer = () => {
+    if (pressTimer.current) {
+      clearTimeout(pressTimer.current);
+      pressTimer.current = null;
+    }
+  };
+
+  const handleClick = () => {
+    if (!longPressTriggered && onClick) {
+      onClick();
+    }
+  };
+
+  useEffect(() => {
+    return () => stopPressTimer(); // Clear any remaining timers on component unmount
+  }, []);
+
   return (
     <div
       className={`overflow-hidden rounded-3xl bg-slate-800 px-4 py-4 ${className}`}
       style={style}
-      onClick={onClick}
+      onClick={handleClick}
+      onMouseDown={startPressTimer}
+      onMouseUp={stopPressTimer}
+      onMouseLeave={stopPressTimer} // Stop timer if mouse leaves element
+      onTouchStart={startPressTimer}
+      onTouchEnd={stopPressTimer}
     >
       {children}
     </div>
