3f81ae819dbb150b9877717879854ce6254df433
rensknoors <rensknoors@gmail.com>
2023-09-30 15:43:26 +0200

    feat: added label prop and graph to graphCard

--- src/components/molecules/GraphCard/GraphCard.tsx
+++ src/components/molecules/GraphCard/GraphCard.tsx
@@ -1,3 +1,4 @@
+import { SvgGraph } from '@hakit/components';
 import { EntityName, useEntity } from '@hakit/core';
 
 import { Card, CardProps } from '@/components/atoms/Card/Card';
@@ -5,19 +6,29 @@ import { Label } from '@/components/atoms/Label/Label';
 
 export type GraphCardProps = {
   entity: EntityName;
+  label?: string;
   unit?: string;
 } & CardProps;
 
-const GraphCard = ({ entity, unit }: GraphCardProps) => {
-  const { state, attributes, custom } = useEntity(entity);
+const GraphCard = ({ entity, label, unit }: GraphCardProps) => {
+  const { state, attributes, custom, history } = useEntity(entity);
 
   return (
-    <Card className="flex flex-col place-items-start bg-slate-300 text-black">
-      <Label className="mb-6 text-slate-300">{attributes.friendly_name}</Label>
+    <Card className="relative flex flex-col place-items-start bg-slate-400 text-black">
+      <Label className="mb-6 text-slate-400">
+        {label ?? attributes.friendly_name}
+      </Label>
       <span className="text-4xl font-semibold">
         {`${state} ${unit ?? attributes.unit_of_measurement}`}
       </span>
-      <span className="">{custom.relativeTime}</span>
+      <span className="text-sm">{custom.relativeTime}</span>
+      <div className="absolute bottom-0 left-0">
+        {history.loading ? (
+          <span>Loading...</span>
+        ) : (
+          <SvgGraph coordinates={history.coordinates} strokeWidth={2} />
+        )}
+      </div>
     </Card>
   );
 };
