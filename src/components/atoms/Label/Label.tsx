b25f511e89a112e718c9dda78b46241e7fe00364
rensknoors <rensknoors@gmail.com>
2023-09-30 15:42:16 +0200

    style: changed styling of label

--- src/components/atoms/Label/Label.tsx
+++ src/components/atoms/Label/Label.tsx
@@ -4,12 +4,7 @@ export type LabelProps = React.HTMLAttributes<HTMLDivElement>;
 
 const Label = ({ children, className }: LabelProps) => {
   return (
-    <div
-      className={clsx(
-        className,
-        'rounded-full bg-black px-4 py-2 text-base text-sm font-semibold'
-      )}
-    >
+    <div className={clsx(className, 'rounded-full bg-black px-4 py-2 text-sm')}>
       {children}
     </div>
   );
