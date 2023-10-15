063aa9cc7ef30a719e0a5844a264d87c3b1423e9
rensknoors <rensknoors@gmail.com>
2023-10-16 01:15:28 +0200

    refactor: moved types

---
+++ src/components/molecules/CalendarCard/types.ts
@@ -0,0 +1,211 @@
+export type ConfigApiCalendar = {
+  apiKey: string;
+  clientId: string;
+  discoveryDocs: string[];
+  scope: string;
+  hosted_domain?: string;
+};
+
+export type TokenClient = {
+  callback: (resp) => void;
+  error_callback: (resp) => void;
+  requestAccessToken: (options: { prompt: string }) => void;
+};
+
+export type EventsList = {
+  kind: 'calendar#events';
+  etag: string;
+  summary: string;
+  description: string;
+  updated: Date;
+  timeZone: string;
+  accessRole: string;
+  defaultReminders: [
+    {
+      method: string;
+      minutes: number;
+    },
+  ];
+  nextPageToken: string;
+  nextSyncToken: string;
+  items: Array<CalendarEvent>;
+};
+
+export type CalendarEvent = {
+  kind: 'calendar#event';
+  etag: string;
+  id: string;
+  status: string;
+  htmlLink: string;
+  created: Date;
+  updated: Date;
+  summary: string;
+  description: string;
+  location: string;
+  colorId: string;
+  creator: {
+    id: string;
+    email: string;
+    displayName: string;
+    self: boolean;
+  };
+  organizer: {
+    id: string;
+    email: string;
+    displayName: string;
+    self: boolean;
+  };
+  start: {
+    date: Date;
+    dateTime: Date;
+    timeZone: string;
+  };
+  end: {
+    date: Date;
+    dateTime: Date;
+    timeZone: string;
+  };
+  endTimeUnspecified: boolean;
+  recurrence: [string];
+  recurringEventId: string;
+  originalStartTime: {
+    date: Date;
+    dateTime: Date;
+    timeZone: string;
+  };
+  transparency: string;
+  visibility: string;
+  iCalUID: string;
+  sequence: number;
+  attendees: [
+    {
+      id: string;
+      email: string;
+      displayName: string;
+      organizer: boolean;
+      self: boolean;
+      resource: boolean;
+      optional: boolean;
+      responseStatus: string;
+      comment: string;
+      additionalGuests: number;
+    },
+  ];
+  attendeesOmitted: boolean;
+  extendedProperties: {
+    private: {
+      (key): string;
+    };
+    shared: {
+      (key): string;
+    };
+  };
+  hangoutLink: string;
+  conferenceData: {
+    createRequest: {
+      requestId: string;
+      conferenceSolutionKey: {
+        type: string;
+      };
+      status: {
+        statusCode: string;
+      };
+    };
+    entryPoints: [
+      {
+        entryPointType: string;
+        uri: string;
+        label: string;
+        pin: string;
+        accessCode: string;
+        meetingCode: string;
+        passcode: string;
+        password: string;
+      },
+    ];
+    conferenceSolution: {
+      key: {
+        type: string;
+      };
+      name: string;
+      iconUri: string;
+    };
+    conferenceId: string;
+    signature: string;
+    notes: string;
+  };
+  gadget: {
+    type: string;
+    title: string;
+    link: string;
+    iconLink: string;
+    width: number;
+    height: number;
+    display: string;
+    preferences: {
+      (key): string;
+    };
+  };
+  anyoneCanAddSelf: boolean;
+  guestsCanInviteOthers: boolean;
+  guestsCanModify: boolean;
+  guestsCanSeeOtherGuests: boolean;
+  privateCopy: boolean;
+  locked: boolean;
+  reminders: {
+    useDefault: boolean;
+    overrides: [
+      {
+        method: string;
+        minutes: number;
+      },
+    ];
+  };
+  source: {
+    url: string;
+    title: string;
+  };
+  workingLocationProperties: {
+    type: string;
+    homeOffice: number;
+    customLocation: {
+      label: string;
+    };
+    officeLocation: {
+      buildingId: string;
+      floorId: string;
+      floorSectionId: string;
+      deskId: string;
+      label: string;
+    };
+  };
+  attachments: [
+    {
+      fileUrl: string;
+      title: string;
+      mimeType: string;
+      iconLink: string;
+      fileId: string;
+    },
+  ];
+  eventType: string;
+};
+
+export type EventsListOptions = {
+  alwaysIncludeEmail?: boolean;
+  // Currently, the array shape does not support all possible combinations of event types.
+  // These might be added later on to Google Calendar API.
+  eventTypes?:
+    | 'default'
+    | 'focusTime'
+    | 'outOfOffice'
+    | 'workingLocation'
+    | ['default', 'focusTime', 'outOfOffice']
+    | ['default', 'focusTime', 'outOfOffice', 'workingLocation']
+    | ['workingLocation'];
+  date?: string;
+  showDeleted?: boolean;
+  singleEvents?: boolean;
+  maxResults?: number;
+  orderBy?: 'startTime' | 'updated';
+};
