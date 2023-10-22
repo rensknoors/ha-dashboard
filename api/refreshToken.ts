175c6970777f0e90bb58d34bdbfda2f7459fe1f3
rensknoors <rensknoors@gmail.com>
2023-10-22 23:41:27 +0200

    feat: add token refresh service

--- api/refreshToken.ts
+++ api/refreshToken.ts
@@ -1,8 +1,40 @@
-module.exports = async (req, res) => {
-  const refreshToken = req.body.refreshToken;
+import cors from 'cors';
+import express from 'express';
+import { OAuth2Client, UserRefreshClient } from 'google-auth-library';
 
-  console.log('refreshToken', refreshToken);
+const app = express();
 
-  // Use the refresh token to obtain a new access token from Google Identity Service
-  res.json({ accessToken: 'new_access_token_here' });
-};
+app.use(cors());
+app.use(express.json());
+
+const oAuth2Client = new OAuth2Client(
+  process.env.VITE_CLIENT_ID,
+  process.env.VITE_CLIENT_SECRET,
+  'postmessage'
+);
+
+app.post('/auth/google', async (req, res) => {
+  try {
+    const { tokens } = await oAuth2Client.getToken(req.body.code);
+    console.log(tokens);
+    res.json(tokens);
+  } catch (error) {
+    res.status(500).send('Error retrieving tokens.');
+  }
+});
+
+app.post('/auth/google/refresh-token', async (req, res) => {
+  try {
+    const user = new UserRefreshClient(
+      process.env.VITE_CLIENT_ID,
+      process.env.VITE_CLIENT_SECRET,
+      req.body.refreshToken
+    );
+    const { credentials } = await user.refreshAccessToken();
+    res.json(credentials);
+  } catch (error) {
+    res.status(500).send('Error refreshing token.');
+  }
+});
+
+app.listen(3001, () => console.log(`Server is running on port 3001`));
