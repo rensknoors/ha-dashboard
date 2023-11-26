f2522a3b856e23243534dee16f52b5b37b0d5784
rensknoors <rensknoors@gmail.com>
2023-11-26 18:28:42 +0100

    refactor: simplify aliases

--- vite.config.ts
+++ vite.config.ts
@@ -19,29 +19,8 @@ export default defineConfig({
     environment: 'happy-dom',
   },
   resolve: {
-    alias: [
-      {
-        find: '@/',
-        replacement: fileURLToPath(new URL('./src', import.meta.url)),
-      },
-      {
-        find: '@/components',
-        replacement: fileURLToPath(
-          new URL('./src/components', import.meta.url)
-        ),
-      },
-      {
-        find: '@/hooks',
-        replacement: fileURLToPath(new URL('./src/hooks', import.meta.url)),
-      },
-      {
-        find: '@/routes',
-        replacement: fileURLToPath(new URL('./src/routes', import.meta.url)),
-      },
-      {
-        find: '@/assets',
-        replacement: fileURLToPath(new URL('./src/assets', import.meta.url)),
-      },
-    ],
+    alias: {
+      '@/': `${fileURLToPath(new URL('./src/', import.meta.url))}/`,
+    },
   },
 });
